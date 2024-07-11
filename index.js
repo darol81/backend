03a864c9d7c08a95bb949c415bea9596f8fb41dc
Topi Mansikkaviita <AC8407@student.labranet.jamk.fi>
2024-07-11 18:56:37 +0300

    Puhelinluettelo backend, steps 1-6 (teht3.1-3.6)

---
+++ index.js
@@ -0,0 +1,113 @@
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = 
+[
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": "1"
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": "2"
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": "3"
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": "4"
+    }
+];
+
+/* ROUTES, main */
+
+/*
+app.get("/", (request, response) => 
+{
+});
+
+*/
+
+/* ROUTES, post */
+
+app.post("/api/persons", (request, response) => 
+{  
+    const body = request.body;
+
+    if (!body.name || !body.number) 
+    {
+        return response.status(400).json({ error: "name or number missing." });
+    }
+
+    if(persons.map(person => person.name).some(name => name === body.name))
+    {
+        return response.status(400).json({ error: "name must be unique." });
+    }
+    
+    const person = 
+    {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 100000000)
+    }
+    persons = persons.concat(person);    
+    response.json(person);
+});
+    
+
+/* GET routes */
+
+app.get("/info", (request, response) => 
+{   
+    date_time = new Date();
+    info = `<p>Phonebook has info for ${persons.length} people</p>
+            <p>${date_time}</p>`;
+    response.send(info);
+});
+
+app.get("/api/persons", (request, response) => 
+{
+    response.json(persons);
+});
+   
+
+app.get("/api/persons/:id", (request, response) => 
+{
+    const id = request.params.id;
+    const person = persons.find(person => person.id === id);
+    
+    if(person)
+    {    
+        response.json(person);
+    } 
+    else 
+    {    
+        response.status(404).end();
+    }
+});
+    
+/* ROUTES, delete */
+app.delete("/api/persons/:id", (request, response) => 
+{
+    const id = request.params.id;
+    persons = persons.filter(person => person.id !== id);
+    response.status(204).end();
+});
+
+/* Pääohjelma */
+
+const PORT = 3001;
+
+app.listen(PORT, () => 
+{
+    console.log(`Server running on port ${PORT}`);
+});

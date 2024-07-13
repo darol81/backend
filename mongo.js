38ca96755ed229f8b6a66fef9f4ff8c1605710c9
Topi Mansikkaviita <AC8407@student.labranet.jamk.fi>
2024-07-13 21:41:58 +0300

    mongo.js, 3.12, tietokanta komentoriviltä

---
+++ mongo.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+if(process.argv.length < 3)
+{
+    console.log("give password as argument");
+    process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://fullstack:${password}@cluster0.kcfxst6.mongodb.net/PhoneApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const phoneSchema = new mongoose.Schema
+({
+    name: String,
+    number: String
+});
+
+const addPerson = (name, number) => 
+{
+    const person = new Person
+    ({
+        name: name,
+        number: number
+    });
+    person.save().then(result => 
+    {
+        console.log(`added ${name} number ${number} to phonebook`);
+        mongoose.connection.close();
+    }); 
+}
+
+
+const Person = mongoose.model("Person", phoneSchema);
+
+if(process.argv.length == 3)
+{
+    console.log("phonebook:");
+    Person.find({}).then(result => 
+    {
+        result.forEach(person => 
+        {
+            console.log(`${person.name} ${person.number}`)
+        });
+        mongoose.connection.close();
+    });
+}
+else
+{
+    addPerson(process.argv[3], process.argv[4]);
+}
+
